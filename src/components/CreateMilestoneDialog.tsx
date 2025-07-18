69b4af0b54ea39aef3b0e5dc5efcbbb3dc82d270
chetansource <chetan.141k@gmail.com>
2025-07-18 17:39:15 +0530

    fixed some bugs

--- src/components/CreateMilestoneDialog.tsx
+++ src/components/CreateMilestoneDialog.tsx
@@ -238,6 +238,7 @@ const CreateMilestoneDialog = forwardRef<
         await sendNotification();
 
         toast({ title: "Milestone created successfully" });
+        console.log("Milestone saved. Closing dialog.");
         onMilestoneCreated?.();
       }
 
